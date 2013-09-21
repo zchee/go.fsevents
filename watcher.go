8c1a3a451299a43e1f74c97e5526368cff514e1b
Ron Burch <ronald.burch@stud.hslu.ch>
2013-09-21 21:13:50 +0200

    fix tests

--- watcher.go
+++ watcher.go
@@ -13,111 +13,111 @@ import "C"
 import "unsafe"
 
 const (
-  FlagUnmount = uint32(C.kFSEventStreamEventFlagUnmount)
-  FlagMount   = uint32(C.kFSEventStreamEventFlagMount)
+	FlagUnmount = uint32(C.kFSEventStreamEventFlagUnmount)
+	FlagMount   = uint32(C.kFSEventStreamEventFlagMount)
 
-  FlagItemCreated       = uint32(C.kFSEventStreamEventFlagItemCreated)
-  FlagItemRemoved       = uint32(C.kFSEventStreamEventFlagItemRemoved)
-  FlagItemInodeMetaMod  = uint32(C.kFSEventStreamEventFlagItemInodeMetaMod)
-  FlagItemRenamed       = uint32(C.kFSEventStreamEventFlagItemRenamed)
-  FlagItemModified      = uint32(C.kFSEventStreamEventFlagItemModified)
-  FlagItemFinderInfoMod = uint32(C.kFSEventStreamEventFlagItemFinderInfoMod)
-  FlagItemChangeOwner   = uint32(C.kFSEventStreamEventFlagItemChangeOwner)
-  FlagItemXattrMod      = uint32(C.kFSEventStreamEventFlagItemXattrMod)
+	FlagItemCreated       = uint32(C.kFSEventStreamEventFlagItemCreated)
+	FlagItemRemoved       = uint32(C.kFSEventStreamEventFlagItemRemoved)
+	FlagItemInodeMetaMod  = uint32(C.kFSEventStreamEventFlagItemInodeMetaMod)
+	FlagItemRenamed       = uint32(C.kFSEventStreamEventFlagItemRenamed)
+	FlagItemModified      = uint32(C.kFSEventStreamEventFlagItemModified)
+	FlagItemFinderInfoMod = uint32(C.kFSEventStreamEventFlagItemFinderInfoMod)
+	FlagItemChangeOwner   = uint32(C.kFSEventStreamEventFlagItemChangeOwner)
+	FlagItemXattrMod      = uint32(C.kFSEventStreamEventFlagItemXattrMod)
 
-  FlagItemIsFile    = uint32(C.kFSEventStreamEventFlagItemIsFile)
-  FlagItemIsDir     = uint32(C.kFSEventStreamEventFlagItemIsDir)
-  FlagItemIsSymlink = uint32(C.kFSEventStreamEventFlagItemIsSymlink)
+	FlagItemIsFile    = uint32(C.kFSEventStreamEventFlagItemIsFile)
+	FlagItemIsDir     = uint32(C.kFSEventStreamEventFlagItemIsDir)
+	FlagItemIsSymlink = uint32(C.kFSEventStreamEventFlagItemIsSymlink)
 )
 
 type watchingInfo struct {
-  channel chan []PathEvent
-  runloop C.CFRunLoopRef
+	channel chan []PathEvent
+	runloop C.CFRunLoopRef
 }
 
 var watchers = make(map[C.FSEventStreamRef]watchingInfo)
 
 type PathEvent struct {
-  Path  string
-  Flags uint32
+	Path  string
+	Flags uint32
 }
 
 func Unwatch(ch chan []PathEvent) {
-  for stream, info := range watchers {
-    if ch == info.channel {
-      C.FSEventStreamStop(stream)
-      C.FSEventStreamInvalidate(stream)
-      C.FSEventStreamRelease(stream)
-      C.CFRunLoopStop(info.runloop)
-    }
-  }
+	for stream, info := range watchers {
+		if ch == info.channel {
+			C.FSEventStreamStop(stream)
+			C.FSEventStreamInvalidate(stream)
+			C.FSEventStreamRelease(stream)
+			C.CFRunLoopStop(info.runloop)
+		}
+	}
 }
 
 func WatchPaths(paths []string) chan []PathEvent {
-  type watchSuccessData struct {
-    runloop C.CFRunLoopRef
-    stream  C.FSEventStreamRef
-  }
+	type watchSuccessData struct {
+		runloop C.CFRunLoopRef
+		stream  C.FSEventStreamRef
+	}
 
-  successChan := make(chan *watchSuccessData)
+	successChan := make(chan *watchSuccessData)
 
-  go func() {
-    pathsToWatch := C.fswatch_make_mutable_array()
-    defer C.CFRelease(C.CFTypeRef(pathsToWatch))
+	go func() {
+		pathsToWatch := C.fswatch_make_mutable_array()
+		defer C.CFRelease(C.CFTypeRef(pathsToWatch))
 
-    for _, dir := range paths {
-      path := C.CString(dir)
-      defer C.free(unsafe.Pointer(path))
+		for _, dir := range paths {
+			path := C.CString(dir)
+			defer C.free(unsafe.Pointer(path))
 
-      str := C.CFStringCreateWithCString(nil, path, C.kCFStringEncodingUTF8)
-      C.CFArrayAppendValue(pathsToWatch, unsafe.Pointer(str))
-    }
+			str := C.CFStringCreateWithCString(nil, path, C.kCFStringEncodingUTF8)
+			C.CFArrayAppendValue(pathsToWatch, unsafe.Pointer(str))
+		}
 
-    stream := C.fswatch_stream_for_paths(pathsToWatch)
-    C.FSEventStreamScheduleWithRunLoop(stream, C.CFRunLoopGetCurrent(), C.kCFRunLoopCommonModes)
+		stream := C.fswatch_stream_for_paths(pathsToWatch)
+		C.FSEventStreamScheduleWithRunLoop(stream, C.CFRunLoopGetCurrent(), C.kCFRunLoopCommonModes)
 
-    ok := C.FSEventStreamStart(stream) != 0
-    if ok {
-      successChan <- &watchSuccessData{
-        runloop: C.CFRunLoopGetCurrent(),
-        stream:  stream,
-      }
-      C.CFRunLoopRun()
-    } else {
-      successChan <- nil
-    }
-  }()
+		ok := C.FSEventStreamStart(stream) != 0
+		if ok {
+			successChan <- &watchSuccessData{
+				runloop: C.CFRunLoopGetCurrent(),
+				stream:  stream,
+			}
+			C.CFRunLoopRun()
+		} else {
+			successChan <- nil
+		}
+	}()
 
-  watchingData := <-successChan
+	watchingData := <-successChan
 
-  if watchingData == nil {
-    return nil
-  }
+	if watchingData == nil {
+		return nil
+	}
 
-  newChan := make(chan []PathEvent)
-  watchers[watchingData.stream] = watchingInfo{
-    channel: newChan,
-    runloop: watchingData.runloop,
-  }
-  return newChan
+	newChan := make(chan []PathEvent)
+	watchers[watchingData.stream] = watchingInfo{
+		channel: newChan,
+		runloop: watchingData.runloop,
+	}
+	return newChan
 }
 
 //export watchDirsCallback
 func watchDirsCallback(stream C.FSEventStreamRef, count C.size_t, paths **C.char, flags *C.FSEventStreamEventFlags) {
-  var events []PathEvent
+	var events []PathEvent
 
-  for i := 0; i < int(count); i++ {
-    cpaths := uintptr(unsafe.Pointer(paths)) + (uintptr(i) * unsafe.Sizeof(*paths))
-    cpath := *(**C.char)(unsafe.Pointer(cpaths))
+	for i := 0; i < int(count); i++ {
+		cpaths := uintptr(unsafe.Pointer(paths)) + (uintptr(i) * unsafe.Sizeof(*paths))
+		cpath := *(**C.char)(unsafe.Pointer(cpaths))
 
-    cflags := uintptr(unsafe.Pointer(flags)) + (uintptr(i) * unsafe.Sizeof(*flags))
-    cflag := *(*C.FSEventStreamEventFlags)(unsafe.Pointer(cflags))
+		cflags := uintptr(unsafe.Pointer(flags)) + (uintptr(i) * unsafe.Sizeof(*flags))
+		cflag := *(*C.FSEventStreamEventFlags)(unsafe.Pointer(cflags))
 
-    events = append(events, PathEvent{
-      Path:  C.GoString(cpath),
-      Flags: uint32(cflag),
-    })
-  }
+		events = append(events, PathEvent{
+			Path:  C.GoString(cpath),
+			Flags: uint32(cflag),
+		})
+	}
 
-  watchers[stream].channel <- events
+	watchers[stream].channel <- events
 }
--- watcher_test.go
+++ watcher_test.go
@@ -1,87 +1,162 @@
 package fsevents
 
-import "testing"
-import "github.com/sdegutis/go.assert"
+import (
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+
+	"testing"
+)
+import "github.com/couchbaselabs/go.assert"
 
 import (
-  "os"
-  "path/filepath"
-  "time"
+	"os"
+	"time"
 )
 
-func withCreate(action func(string)) {
-  dummyfile := "dummyfile.txt"
-  os.Create(dummyfile)
+func withCreate(base string, action func(string)) {
+	dummyfile := "dummyfile.txt"
+	os.Create(filepath.Join(base, dummyfile))
 
-  action(dummyfile)
+	action(filepath.Join(base, dummyfile))
 
-  os.Remove(dummyfile)
+	os.Remove(filepath.Join(base, dummyfile))
 }
 
 func TestFileChanges(t *testing.T) {
-  ch := WatchPaths([]string{"."})
+	t.Parallel()
+	base, rm := TempDir()
+	defer rm()
 
-  withCreate(func(dummyfile string) {
-    select {
-    case <-ch:
-    case <-time.After(time.Second * 1):
-      t.Errorf("should have got some file event, but timed out")
-    }
-  })
+	ch := WatchPaths([]string{base})
+
+	withCreate(base, func(dummyfile string) {
+		select {
+		case <-ch:
+		case <-time.After(time.Second * 1):
+			t.Errorf("should have got some file event, but timed out")
+		}
+	})
 }
 
 func TestEventFlags(t *testing.T) {
-  ch := WatchPaths([]string{"."})
+	t.Parallel()
+	base, rm := TempDir()
+	defer rm()
 
-  withCreate(func(dummyfile string) {
-    select {
-    case events := <-ch:
-      assert.Equals(t, len(events), 1)
-      assert.True(t, events[0].Flags&FlagItemCreated != 0)
-    case <-time.After(time.Second * 1):
-      t.Errorf("should have got some file event, but timed out")
-    }
-  })
+	ch := WatchPaths([]string{base})
+
+	withCreate(base, func(dummyfile string) {
+		select {
+		case events := <-ch:
+			assert.Equals(t, len(events), 2)
+
+			assert.True(t, events[1].Flags&FlagItemCreated != 0)
+		case <-time.After(time.Second * 1):
+			t.Errorf("should have got some file event, but timed out")
+		}
+	})
 }
 
 func TestCanGetPath(t *testing.T) {
-  ch := WatchPaths([]string{"."})
+	t.Parallel()
+	base, rm := TempDir()
+	defer rm()
 
-  withCreate(func(dummyfile string) {
-    select {
-    case events := <-ch:
-      assert.Equals(t, len(events), 1)
+	ch := WatchPaths([]string{base})
 
-      fullpath, _ := filepath.Abs(dummyfile)
-      assert.Equals(t, events[0].Path, fullpath)
-    case <-time.After(time.Second * 2):
-      t.Errorf("timed out")
-    }
-  })
+	withCreate(base, func(dummyfile string) {
+		select {
+		case events := <-ch:
+			assert.Equals(t, len(events), 2)
+
+			fullpath, _ := filepath.Abs(dummyfile)
+			fullpath, _ = filepath.EvalSymlinks(fullpath)
+			evPath, _ := filepath.EvalSymlinks(events[1].Path)
+			assert.Equals(t, evPath, fullpath)
+		case <-time.After(time.Second * 2):
+			t.Errorf("timed out")
+		}
+	})
 }
 
 func TestOnlyWatchesSpecifiedPaths(t *testing.T) {
-  ch := WatchPaths([]string{"imaginaryfile"})
+	t.Parallel()
+	base, rm := TempDir()
+	defer rm()
 
-  withCreate(func(dummyfile string) {
-    select {
-    case <-ch:
-      t.Errorf("should have timed out, but got some file event")
-    case <-time.After(time.Second * 1):
-    }
-  })
+	ch := WatchPaths([]string{filepath.Join(base, "imaginaryfile")})
+
+	withCreate(base, func(dummyfile string) {
+		select {
+		case <-ch:
+			t.Errorf("should have timed out, but got some file event")
+		case <-time.After(time.Second * 1):
+		}
+	})
 }
 
 func TestCanUnwatch(t *testing.T) {
-  ch := WatchPaths([]string{"."})
+	t.Parallel()
+	base, rm := TempDir()
+	defer rm()
 
-  Unwatch(ch)
+	ch := WatchPaths([]string{base})
 
-  withCreate(func(dummyfile string) {
-    select {
-    case <-ch:
-      t.Errorf("should have timed out, but got some file event")
-    case <-time.After(time.Second * 1):
-    }
-  })
+	Unwatch(ch)
+
+	withCreate(base, func(dummyfile string) {
+		select {
+		case <-ch:
+			t.Errorf("should have timed out, but got some file event")
+		case <-time.After(time.Second * 1):
+		}
+	})
+}
+
+func TestMultipleFile(t *testing.T) {
+	t.Parallel()
+	base, rm := TempDir()
+	defer rm()
+
+	f := make(chan bool)
+	var ch chan ([]PathEvent)
+	go func() {
+		ch = WatchPaths([]string{base})
+		f <- true
+	}()
+	<-f
+	// defer Unwatch(ch)
+
+	files := []string{"holla", "huhu", "heeeho", "haha"}
+	for _, f := range files {
+		ioutil.WriteFile(filepath.Join(base, f), []byte{12, 32}, 0777)
+	}
+
+	events := []string{}
+LOOP:
+	for {
+		select {
+		case e, ok := <-ch:
+			if !ok {
+				break LOOP
+			}
+			for _, item := range e[1:] {
+				p, _ := filepath.Rel(base, item.Path)
+				events = append(events, p)
+			}
+		case <-time.After(time.Second * 5):
+			break LOOP
+		}
+	}
+
+	assert.Equals(t, strings.Join(events, " "), strings.Join(files, " "))
+}
+
+func TempDir() (string, func()) {
+	path, _ := ioutil.TempDir("", "TempDir")
+	path, _ = filepath.EvalSymlinks(path)
+	return path, func() {
+		os.RemoveAll(path)
+	}
 }
